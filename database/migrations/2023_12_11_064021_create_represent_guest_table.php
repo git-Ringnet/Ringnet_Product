5aa4a182fa1524f4278f8f9ff5f6b91d4901a5f1
tuanle1293 <93644812+tuanle1293@users.noreply.github.com>
2023-12-11 13:55:19 +0700

    update database

---
+++ database/migrations/2023_12_11_064021_create_represent_guest_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('represent_guest', function (Blueprint $table) {
+            $table->id();
+            $table->integer('guest_id');
+            $table->string('represent_name');
+            $table->string('represent_email');
+            $table->string('represent_phone');
+            $table->string('represent_address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('represent_guest');
+    }
+};
