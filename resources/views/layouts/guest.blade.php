4519622cf1830d7bade6158b86ec7ebbf54c8535
Nguyễn Quốc Việt <88937231+nqv1710@users.noreply.github.com>
2024-01-26 15:25:57 +0700

    push

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    <meta name="google-site-verification" content="K1w-WrdYbh4UE0ZLNP8lot6xN-tkavJTByQY-Hd4t-0" />
+
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
