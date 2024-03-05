34153b01a54922799557ad87c68f3ed291fab1f7
Thái Minh Hiếu <87421130+HieuTamihi@users.noreply.github.com>
2024-03-05 18:04:28 +0700

    update ma_giao_hang - ma_thanh_toan

--- resources/views/tables/export/pay_export/create-payExport.blade.php
+++ resources/views/tables/export/pay_export/create-payExport.blade.php
@@ -454,8 +454,7 @@
                             <div
                                 class="d-flex align-items-center justify-content-between border border-left-0 py-1 px-1 border-top-0">
                                 <input type="text" placeholder="Nhập thông tin" name="code_payment"
-                                    class="border-0 bg w-100 bg-input-guest py-0 px-0"
-                                    autocomplete="off" required>
+                                    class="border-0 bg w-100 bg-input-guest py-0 px-0" autocomplete="off" required>
                                 <div class="opacity-0">
                                     <svg width="18" height="18" viewBox="0 0 24 24" fill="none"
                                         xmlns="http://www.w3.org/2000/svg">
@@ -929,6 +928,7 @@
                     $('.daThanhToan').val(formatCurrency(data.tongThanhToan));
                     $('.duNo').val(formatCurrency(Math.round(data.tongTienNo - data
                         .tongThanhToan)));
+                    $('input[name="code_payment"]').val('MTT-' + (data.lastPayExportId + 1));
                     $.ajax({
                         url: '{{ route('getProductPay') }}',
                         type: 'GET',
