5eeb1b2e113b8963f58727986f1b06eba20d7458
Nguyễn Quốc Việt <88937231+nqv1710@users.noreply.github.com>
2024-03-06 16:01:52 +0700

    push

--- resources/views/report/index.blade.php
+++ resources/views/report/index.blade.php
@@ -1,6 +1,6 @@
 <x-navbar :title="$title" activeName="report"></x-navbar>
 <!-- Content Wrapper. Contains page content -->
-<div class="content-wrapper m-0 p-0 min-height--none">
+<div class="content-wrapper m-0">
     <!-- Content Header (Page header) -->
     <div class="content-header-fixed p-0 margin-250">
         <div class="content__header--inner margin-left32">
@@ -32,7 +32,6 @@
                     </button>
                     </a>
                     <button type="button" class="btn-option bg-white border-0">
-
                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
                             xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" clip-rule="evenodd"
@@ -49,6 +48,7 @@
                 </div>
             </div>
         </div>
+
         <section class="content-header--options p-0 border-custom">
             <ul class="header-options--nav nav justify-content-evenly nav-tabs margin-left32" style="width: 199px;">
                 <li class="active">
@@ -258,11 +258,11 @@
                 <!-- Main content -->
                 <section class="content margin-250">
                     <div class="container-fluided">
-                        <div class="row p-0 m-0">
+                        <div class="row">
                             <div class="col-md-12 p-0 m-0 pl-2">
-                                <div class="card">
+                                <div class="card scroll-custom mt-3">
                                     <!-- /.card-header -->
-                                    <div class="outer text-nowrap">
+                                    <div class="card-body">
                                         <table id="example2" class="table table-hover">
                                             <thead class="sticky-head">
                                                 <tr>
@@ -371,6 +371,8 @@
                                         </table>
                                     </div>
                                 </div>
+
+
                             </div>
                         </div>
                     </div>
@@ -379,112 +381,16 @@
         </div>
         {{-- Bán hàng --}}
         <div id="export" class="tab-pane fade">
-            <div class="content">
+            <div class="content margin-top-fixed6">
                 <!-- Main content -->
                 <section class="content margin-250">
-                    <div class="export">
-                        <div class="bg-filter-search pl-4">
-                            <div class="content-wrapper1 py-2">
-                                <div class="row m-auto filter p-0">
-                                    <div class="w-100">
-                                        <div class="row mr-0">
-                                            <div class="col-md-5 d-flex align-items-center">
-                                                <form action="" method="get" id="search-filter" class="p-0 m-0">
-                                                    <div class="position-relative ml-1">
-                                                        <input type="text" placeholder="Tìm kiếm" name="keywords"
-                                                            id="search-export" style="outline: none;"
-                                                            class="pr-4 w-100 input-search text-13"
-                                                            value="{{ request()->keywords }}">
-                                                        <span id="search-icon" class="search-icon btn-submit"
-                                                            data-button="export"><i class="fas fa-search"
-                                                                aria-hidden="true"></i></span>
-                                                        <input class="btn-submit" type="submit" id="hidden-submit"
-                                                            name="hidden-submit" style="display: none;">
-                                                    </div>
-                                                </form>
-                                                <div class="dropdown mx-2">
-                                                    <button class="btn-filter_searh" data-toggle="dropdown">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                                            viewBox="0 0 16 16" fill="none">
-                                                            <path
-                                                                d="M12.9548 3H10.0457C9.74445 3 9.50024 3.24421 9.50024 3.54545V6.45455C9.50024 6.75579 9.74445 7 10.0457 7H12.9548C13.256 7 13.5002 6.75579 13.5002 6.45455V3.54545C13.5002 3.24421 13.256 3 12.9548 3Z"
-                                                                fill="#6D7075" />
-                                                            <path
-                                                                d="M6.45455 3H3.54545C3.24421 3 3 3.24421 3 3.54545V6.45455C3 6.75579 3.24421 7 3.54545 7H6.45455C6.75579 7 7 6.75579 7 6.45455V3.54545C7 3.24421 6.75579 3 6.45455 3Z"
-                                                                fill="#6D7075" />
-                                                            <path
-                                                                d="M6.45455 9.50024H3.54545C3.24421 9.50024 3 9.74445 3 10.0457V12.9548C3 13.256 3.24421 13.5002 3.54545 13.5002H6.45455C6.75579 13.5002 7 13.256 7 12.9548V10.0457C7 9.74445 6.75579 9.50024 6.45455 9.50024Z"
-                                                                fill="#6D7075" />
-                                                            <path
-                                                                d="M12.9548 9.50024H10.0457C9.74445 9.50024 9.50024 9.74445 9.50024 10.0457V12.9548C9.50024 13.256 9.74445 13.5002 10.0457 13.5002H12.9548C13.256 13.5002 13.5002 13.256 13.5002 12.9548V10.0457C13.5002 9.74445 13.256 9.50024 12.9548 9.50024Z"
-                                                                fill="#6D7075" />
-                                                        </svg>
-                                                        <span class="text-btnIner">Bộc lọc</span>
-                                                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none"
-                                                            xmlns="http://www.w3.org/2000/svg">
-                                                            <path fill-rule="evenodd" clip-rule="evenodd"
-                                                                d="M5.42342 6.92342C5.65466 6.69219 6.02956 6.69219 6.26079 6.92342L9 9.66264L11.7392 6.92342C11.9704 6.69219 12.3453 6.69219 12.5766 6.92342C12.8078 7.15466 12.8078 7.52956 12.5766 7.76079L9.41868 10.9187C9.18745 11.1499 8.81255 11.1499 8.58132 10.9187L5.42342 7.76079C5.19219 7.52956 5.19219 7.15466 5.42342 6.92342Z"
-                                                                fill="#6B6F76" />
-                                                        </svg>
-                                                    </button>
-                                                    <div class="dropdown-menu" id="dropdown-menu"
-                                                        aria-labelledby="dropdownMenuButton">
-                                                        <div class="search-container px-2">
-                                                            <input type="text" placeholder="Tìm kiếm" id="myInput"
-                                                                class="text-13" onkeyup="filterFunction()"
-                                                                style="outline: none;">
-                                                            <span class="search-icon mr-2">
-                                                                <i class="fas fa-search"></i>
-                                                            </span>
-                                                        </div>
-                                                        <div class="scrollbar">
-                                                            <button class="dropdown-item btndropdown text-13-black btn-code"
-                                                                id="btn-code-export" data-button="code" data-button="export"
-                                                                type="button">Mã khách hàng
-                                                            </button>
-                                                            <button class="dropdown-item btndropdown text-13-black btn-name"
-                                                                id="btn-name-export" data-button="name" data-button="export"
-                                                                type="button">Công
-                                                                ty
-                                                            </button>
-                                                            <button class="dropdown-item btndropdown text-13-black btn-total"
-                                                                id="btn-total-export" data-button="export" data-button="total"
-                                                                type="button">
-                                                                Tổng doanh số
-                                                            </button>
-                                                            <button class="dropdown-item btndropdown text-13-black btn-debt"
-                                                                id="btn-debt-export" data-button="export" data-button="debt"
-                                                                type="button">
-                                                                Công nợ
-                                                            </button>
-                                                        </div>
-                                                    </div>
-                                                    {{-- Bán --}}
-                                                    <x-filter-text name="code-export" button="export" title="Mã khách hàng" />
-                                                    <x-filter-checkbox :dataa='$guests' name="name-export" title="Tên khách hàng"
-                                                        namedisplay="guest_name" />
-                                                    <x-filter-compare name="total-export" title="Tổng doanh số" />
-                                                    <x-filter-compare name="debt-export" title="Công nợ" />
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="border-custom">
-                        <div class="margin-left32 text-13 py-3">
-                            <span>Tổng số doanh thu bán hàng: <span> <= </span></span>
-                            <span>10.000.000.000</span>
-                        </div>
-                    </div>
                     <div class="container-fluided">
-                        <div class="row p-0 m-0">
-                            <div class="col-md-12 p-0 m-0">
-                                <div class="card">
+                        <div class="row">
+                            <div class="col-md-12 p-0 m-0 pl-2">
+
+                                <div class="card scroll-custom mt-3">
                                     <!-- /.card-header -->
-                                    <div class="outer text-nowrap">
+                                    <div class="card-body">
                                         <table id="example2" class="table table-hover">
                                             <thead class="sticky-head">
                                                 <tr>
@@ -861,4 +767,3 @@
 
     });
 </script>
-
