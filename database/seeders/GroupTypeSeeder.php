b5a67e5331eca9d25141149c141b7a5c13483773
Nguyễn Quốc Việt <88937231+nqv1710@users.noreply.github.com>
2024-05-22 09:35:57 +0700

    push

--- database/seeders/GroupTypeSeeder.php
+++ database/seeders/GroupTypeSeeder.php
@@ -6,31 +6,33 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class Workspace extends Seeder
+class GroupTypeSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        DB::table('workspaces')->insert([
+        DB::table('grouptype')->insert([
             [
                 'name' => 'Nhân viên',
                 'description' => 'Nhóm dành cho nhân viên',
-                'created_at' => now(),
-                'updated_at' => now(),
+                'created_at' => now(), 'updated_at' => now(),
             ],
             [
                 'name' => 'Khách hàng',
                 'description' => 'Nhóm dành cho khách hàng',
-                'created_at' => now(),
-                'updated_at' => now(),
+                'created_at' => now(), 'updated_at' => now(),
             ],
             [
                 'name' => 'Nhà cung cấp',
                 'description' => 'Nhóm dành cho nhà cung cấp',
-                'created_at' => now(),
-                'updated_at' => now(),
+                'created_at' => now(), 'updated_at' => now(),
+            ],
+            [
+                'name' => 'Hàng hoá',
+                'description' => 'Nhóm dành cho hàng hoá',
+                'created_at' => now(), 'updated_at' => now(),
             ],
         ]);
     }
