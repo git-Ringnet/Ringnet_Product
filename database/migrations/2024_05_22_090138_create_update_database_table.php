55f171ad37ec45c3ef4013689559760c11a5e17b
tuanle1293 <93644812+tuanle1293@users.noreply.github.com>
2024-05-23 16:18:27 +0700

    update database

---
+++ database/migrations/2024_05_22_090138_create_update_database_table.php
@@ -0,0 +1,157 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // Schema::create('update_database', function (Blueprint $table) {
+        //     $table->id();
+        //     $table->timestamps();
+        // });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        Schema::table('provides', function (Blueprint $table) {
+            $table->decimal('quota_debt', 20, 4)->nullable()->after('provide_phone');
+            $table->integer('category_id')->nullable()->after('provide_phone');
+            $table->string('provide_fax')->nullable()->after('provide_phone');
+        });
+        Schema::table('products', function (Blueprint $table) {
+            $table->decimal('price_retail', 20, 4)->nullable()->after('user_id');
+            $table->decimal('price_wholesale', 20, 4)->nullable()->after('user_id');
+            $table->decimal('price_specialsale', 20, 4)->nullable()->after('user_id');
+            $table->decimal('product_weight', 20, 4)->nullable()->after('user_id');
+            $table->integer('category_id')->nullable()->after('user_id');
+        });
+
+        Schema::create('content-import-export', function (Blueprint $table) {
+            $table->id();
+            $table->string('document')->nullable();
+            $table->string('type')->nullable();
+            $table->string('name')->nullable();
+            $table->string('content');
+            $table->decimal('qty_money', 20, 4)->nullable();
+            $table->integer('fund_id');
+            $table->string('fund_name')->nullable();
+            $table->string('notes')->nullable();
+            $table->integer('user_id')->nullable();
+            $table->integer('workspace_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('update_database');
+    }
+};
